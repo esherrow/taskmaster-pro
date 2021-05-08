b7285fd230a6dec015bfb0e10fac22485e67ab0b
Yoga Cats <esherrow77@gmail.com>
2021-05-08 10:10:12 -0400

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,6 +57,7 @@ var auditTask = function(taskEl){
   else if(Math.abs(moment().diff(time, "days"))<=2){
     $(taskEl).addClass("list-group-item-warning");
   }
+  console.log(taskEl);
 };
 
 $(".list-group").on("click", "p", function() {
@@ -131,16 +132,22 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance:"pointer",
   helper: "clone",
-  activate:function(event){
+  activate:function(event, ui){
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
     console.log("activate", this);
   },
-  deactivate: function(event){
+  deactivate: function(event, ui){
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
     console.log("deactivate", this);
   },
   over: function(event){
+    $(event.target).addClass("dropover-active");
     console.log("over", event.target);
   },
   out: function(event){
+    $(event.target).removeClass("dropover-active");
     console.log("out", event.target);
   },
   update: function(event){
@@ -176,9 +183,11 @@ $("#trash").droppable({
     console.log("drop");
   },
   over: function(event, ui){
+    $(".bottom-trash").addClass("bottom-trash-active");
     console.log("over");
   },
   out: function(event, ui) {
+    $(".bottom-trash").removeClass("bottom-trash-active");
     console.log("out");
   }
 });
@@ -200,7 +209,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -232,5 +241,10 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
-
+ 
+setInterval(function(){
+  $(".card .list-group-item").each(function(index, el){
+    auditTask(el);
+  });
+}, (1000*60*30));
 
